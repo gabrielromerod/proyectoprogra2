74156f9972fce83cfda2da67c403b4889465f89e
Gabriel Romero <101651659+gabrielromerod@users.noreply.github.com>
2022-11-19 21:46:00 +0000

    estable 2

--- final.cpp
+++ final.cpp
@@ -1,1 +1,16 @@
-#include 
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+    // Create a file object
+    ofstream file;
+    // Open the file
+    file.open("test.txt");
+    // Write to the file
+    file << "Hello World 2" << endl;
+    // Close the file
+    file.close();
+};
